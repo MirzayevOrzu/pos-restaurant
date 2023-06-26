08d0f0a1e8b52854ba8d0a1bb01fb74434105380
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-27 00:25:43 +0500

    validate

--- src/modules/users/controllers.js
+++ src/modules/users/controllers.js
@@ -3,6 +3,8 @@ import { listUsers } from './list-users.js';
 import { showUser } from './show-user.js';
 import { editUser } from './edit-user.js';
 import { removeUser } from './remove-user.js';
+import httpValidator from '../../shared/http-validator/index.js';
+import { deleteUserSchema, getUserSchema, patchUserSchema } from './schemas.js';
 
 /**
  * @param {express.Request} req
@@ -26,6 +28,8 @@ export const getUsers = async (req, res, next) => {
  */
 export const getUser = async (req, res, next) => {
   try {
+    httpValidator({ params: req.params }, getUserSchema)();
+
     const result = await showUser({ id: req.params.id });
 
     res.status(200).json(result);
@@ -41,6 +45,8 @@ export const getUser = async (req, res, next) => {
  */
 export const patchUser = async (req, res, next) => {
   try {
+    httpValidator({ params: req.params, body: req.body }, patchUserSchema)();
+
     const result = await editUser({ id: req.params.id, ...req.body });
 
     res.status(200).json(result);
@@ -56,6 +62,8 @@ export const patchUser = async (req, res, next) => {
  */
 export const deleteUser = async (req, res, next) => {
   try {
+    httpValidator({ params: req.params }, deleteUserSchema)();
+
     const result = await removeUser({ id: req.params.id });
 
     res.status(200).json(result);
---
+++ src/modules/users/schemas.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+
+export const getUserSchema = {
+  params: Joi.object({
+    id: Joi.number().integer().required(),
+  }),
+};
+
+export const patchUserSchema = {
+  params: Joi.object({
+    id: Joi.number().integer().required(),
+  }),
+  body: Joi.object({
+    first_name: Joi.string().trim().min(1),
+    last_name: Joi.string().trim().min(1),
+    username: Joi.string().trim().min(3),
+    password: Joi.string().min(3),
+  }),
+};
+
+export const deleteUserSchema = {
+  params: Joi.object({
+    id: Joi.number().integer().required(),
+  }),
+};
