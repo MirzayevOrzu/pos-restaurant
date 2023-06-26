2c97ad5eef62cf16951ca842cc7e502fb5cff8bc
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-26 23:46:23 +0500

    handle errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from './shared/config/index.js';
+import handleErrors from './shared/errors/handle.js';
 import api from './modules/api.js';
 
 const app = express();
@@ -11,6 +12,8 @@ app.use('/api', api);
 app.get('/', (req, res) => {
   res.sendStatus(200);
 });
+
+app.use(handleErrors);
 
 app.listen(config.port, () => {
   console.log(`Server is listening on port ${config.port}`);
