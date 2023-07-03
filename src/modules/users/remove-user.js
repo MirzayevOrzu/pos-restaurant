0a08b2dd9c218e1caa2a3978d2fa50b8ca70d79c
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-07-03 18:41:08 +0500

    fix(users): remove user

--- src/modules/users/remove-user.js
+++ src/modules/users/remove-user.js
@@ -2,13 +2,13 @@ import db from '../../db/index.js';
 import { NotFoundError } from '../../shared/errors/index.js';
 
 export const removeUser = async ({ id }) => {
-  const user = await db('users').where({ id }).first();
+  const user = await db('users').where({ id, is_deleted: false }).first();
 
   if (!user) {
     throw new NotFoundError('Foydalanuvchi topilmadi.');
   }
 
-  const result = await db('users').where({ id }).update({ is_deleted: true });
+  const result = await db('users').where({ id }).update({ is_deleted: true }).returning('*');
 
-  return result;
+  return result[0];
 };
