be29f0a4498c87a80e7fb1165cd4b729133bd5aa
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-07-03 18:06:31 +0500

    fix(items): rm joins from item use cases

--- src/modules/items/list-items.js
+++ src/modules/items/list-items.js
@@ -1,25 +1,9 @@
 import db from '../../db/index.js';
 
 export const listItems = async () => {
-  const dbQuery = db('items')
-    .where({ 'items.is_deleted': false, 'item_options.is_deleted': false })
-    .innerJoin('item_options', 'items.id', 'item_options.item_id')
-    .select(
-      'items.*',
-      db.raw(`
-      json_agg(
-        json_build_object(
-          'id', item_options.id,
-          'item_id', item_options.item_id,
-          'measurement_id', item_options.measurement_id,
-          'unit', item_options.unit,
-          'price', item_options.price
-        )
-      ) as options
-      `)
-    );
+  const dbQuery = db('items').where({ is_deleted: false }).select('*');
 
-  const items = await dbQuery.groupBy('items.id');
+  const items = await dbQuery;
 
   return items;
 };
--- src/modules/items/show-item.js
+++ src/modules/items/show-item.js
@@ -2,29 +2,11 @@ import db from '../../db/index.js';
 import { NotFoundError } from '../../shared/errors/index.js';
 
 export const showItem = async ({ id }) => {
-  const category = await db('items')
-    .where({ 'items.id': id, 'items.is_deleted': false, 'item_options.is_deleted': false })
-    .innerJoin('item_options', 'items.id', 'item_options.item_id')
-    .select(
-      'items.*',
-      db.raw(`
-      json_agg(
-        json_build_object(
-          'id', item_options.id,
-          'item_id', item_options.item_id,
-          'measurement_id', item_options.measurement_id,
-          'unit', item_options.unit,
-          'price', item_options.price
-        )
-      ) as options
-    `)
-    )
-    .groupBy('items.id')
-    .first();
+  const item = await db('items').where({ id: id, is_deleted: false }).select('*').first();
 
-  if (!category) {
+  if (!item) {
     throw new NotFoundError('Tovar topilmadi.');
   }
 
-  return category;
+  return item;
 };
