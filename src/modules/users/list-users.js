aebe188c2bf6fb872f8bbf2000567cf03c40acba
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-07-03 11:10:59 +0500

    fix(users): simplify list users

--- src/modules/users/list-users.js
+++ src/modules/users/list-users.js
@@ -3,11 +3,7 @@ import db from '../../db/index.js';
 export const listUsers = async () => {
   const dbQuery = db('users').where({ is_deleted: false }).select('*');
 
-  const count = await dbQuery.clone().count().groupBy('id');
   const users = await dbQuery;
 
-  return {
-    count: count.length,
-    list: users,
-  };
+  return users
 };
