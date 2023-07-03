1361cbdb1e4b30794f987729cfb70e031cc9c183
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-07-03 11:30:06 +0500

    fix(measurements): simplify list measurements

--- src/modules/measurements/list-measurements.js
+++ src/modules/measurements/list-measurements.js
@@ -3,11 +3,7 @@ import db from '../../db/index.js';
 export const listMeasurements = async () => {
   const dbQuery = db('measurements').where({ is_deleted: false }).select('*');
 
-  const count = await dbQuery.clone().count().groupBy('id');
   const measurements = await dbQuery;
 
-  return {
-    count: count.length,
-    list: measurements,
-  };
+  return measurements;
 };
