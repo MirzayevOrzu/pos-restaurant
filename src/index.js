c34e3eda0c01f4e99aeb8a9955b402f30949b14a
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-07-07 20:42:11 +0500

    chore: start with express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import config from './shared/config/index.js';
 import typeDefs from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers.js';
 
+const app = express();
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+await server.start();
 
-const { url } = await startStandaloneServer(server, { listen: { port: config.port } });
-console.log(`Server is listening on url ${url}`);
+app.use('/graphql', cors(), express.json(), expressMiddleware(server));
+
+await new Promise((resolve) => httpServer.listen({ port: config.port }, resolve));
+console.log(`Server is listening on port ${config.port}`);
